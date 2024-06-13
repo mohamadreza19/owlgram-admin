f8d6cb4b927c3b76acee14b590459e810b0951c5
mrzar <mrzar1380@gmail.com>
2024-06-13 23:20:31 +0330

    add placeholder to dashboard

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,9 @@
 "use client";
 
 export default function Home() {
-  return <div className="w-full">123</div>;
+  return (
+    <div className="w-full container">
+      <h2 className="font-extrabold text-center">Chose Item In Left Menu</h2>
+    </div>
+  );
 }
